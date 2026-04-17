0f643f87613d2ea6bb13e9b19eff2190aa31134c
liguobao <codelover@qq.com>
2026-04-17 23:55:52 +0800

    chore: upgrade log4js 1.x to 6.x (node 22 upgrade phase 6)
    log4js 6 overhauled its configure() shape:
    - appenders is now a named object (not an array)
    - categories is required to wire appenders to logger names
    - logger.setLevel() replaced by category-level config
    
    Migrate app/utils/logger.js accordingly. This removes the shelljs
    circular-dependency warnings that log4js 1.x pulled in via its
    old streamroller dependency.

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -4,17 +4,25 @@ import config from 'config'
 
 const logConfig = config.get('log')
 const appName = config.get('appName')
+const category = `[${appName.toUpperCase()}]`
 
-// Create a deep copy to avoid immutability issues with newer config versions
-const logAppenderCopy = JSON.parse(JSON.stringify(logConfig.appender))
-
+// log4js 6.x: appenders is a named map, categories wires them up
 log4js.configure({
-  appenders: [
-    logAppenderCopy
-  ]
+  appenders: {
+    out: { ...logConfig.appender }
+  },
+  categories: {
+    default: {
+      appenders: ['out'],
+      level: logConfig.level
+    },
+    [category]: {
+      appenders: ['out'],
+      level: logConfig.level
+    }
+  }
 })
 
-const logger = log4js.getLogger(`[${appName.toUpperCase()}]`)
-logger.setLevel(logConfig.level)
+const logger = log4js.getLogger(category)
 
 export default logger
